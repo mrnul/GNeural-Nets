c2fe433856295ffb1350be13f0ba23fdd18ae9a2
mrnul <16276083+mrnul@users.noreply.github.com>
2020-05-24 16:58:16 +0300

    Update example.cpp

--- examples/example.cpp
+++ examples/example.cpp
@@ -18,12 +18,15 @@ int main()
 	NetworkWithInfo res;
 	GAGNN Test(Net, 10, 2, 1.0f, 1.0f, 1000, 1);
 
+	cout.precision(3);
 	while (res.Network.Info.Error > 0.01f)
 	{
-		res = Test.CalcNextGeneration(2, 0.1f, input, output);
-		cout << res.Network.Info.Error << " |-> ";
-		cout << res.Network.Info.Accuracy << " " << res.ExInfo.Generation << " " << res.Network.Info.NumberOfWeights << " ";
+		res = Test.CalcNextGeneration(2, 0.1f, input, output, 10);
+		cout << res.Network.Info.Error << "\t|->\t";
+		cout << res.Network.Info.Accuracy << "\t" << res.ExInfo.Generation << "\t" << res.Network.Info.NumberOfWeights << "\t";
+		cout << Test.KillEliteAtRandom(0.1f, 1.0f);
 		cout << endl;
+		
 	}
 	cout << endl;
 	for (int i = 0; i < input.size(); i++)
