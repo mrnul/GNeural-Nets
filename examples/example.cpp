ff1363a443a3b7945e7d435d6ce9e3b7e3af76bb
mrnul <16276083+mrnul@users.noreply.github.com>
2020-05-31 16:04:16 +0300

    Add files via upload

--- examples/example.cpp
+++ examples/example.cpp
@@ -7,24 +7,30 @@ using std::endl;
 
 int main()
 {
-	const vector<vector<float>> input =  {{1,0}, {1,1}, {0,1}, {0,0}};
-	const vector<vector<float>> output = {{1,0}, {0,1}, {1,0}, {0,1}};
+	const vector<vector<float>> input = { {1,0}, {1,1}, {0,1}, {0,0} };
+	const vector<vector<float>> output = { {1,0}, {0,1}, {1,0}, {0,1} };
 
 	const vector<int> topology = { (int)input[0].size() , 2 , (int)output[0].size() };
 
-	
-	GAGNN Test(GNeuralNetwork(topology), 20, 8, 1.0f, 1.0f, 100, 1);
+
+	GAGNNParams Params;
+	Params.MaxGen = 30;
+	Params.MutationProb = 0.2f;
+	Params.ParentCount = 2;
+
+	GAGNN Test(GNeuralNetwork(topology), 10, 4, 1.0f, 100, 1);
 	NetworkWithInfo res;
 
 	cout.precision(3);
 	while (res.Network.Info.Error > 0.01f)
 	{
-		res = Test.CalcNextGeneration(2, 0.1f, input, output);
+		Test.CalcNextGeneration(Params, input, output);
+		res = Test.GetBest();
 		cout << res.Network.Info.Error << "\t|->\t";
 		cout << res.Network.Info.Accuracy << "\t" << res.ExInfo.Generation << "\t" << res.Network.Info.NumberOfWeights << "\t";
-		cout << Test.KillEliteAtRandom(0.1f, 1.0f);
+		cout << Test.KillEliteAtRandom(0.01f);
 		cout << endl;
-		
+
 	}
 	cout << endl;
 	for (int i = 0; i < input.size(); i++)
