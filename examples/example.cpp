84b8b671ae567b0d9db9367d19b2d33471d05349
mrnul <16276083+mrnul@users.noreply.github.com>
2020-06-06 21:32:05 +0300

    Add files via upload

--- examples/example.cpp
+++ examples/example.cpp
@@ -7,18 +7,27 @@ using std::endl;
 
 int main()
 {
+	//XOR inputs
 	const vector<vector<float>> input = { {1,0}, {1,1}, {0,1}, {0,0} };
+
+	//{1,0| = True
+	//{0,1} = False
 	const vector<vector<float>> output = { {1,0}, {0,1}, {1,0}, {0,1} };
 
-	const vector<int> topology = { (int)input[0].size() , 2 , (int)output[0].size() };
-
+	// 2 input nodes, one hidden layer with 2 nodes, 2 output nodes
+	const vector<int> topology = { 2 , 2 , 2 };
 
 	GAGNNParams Params;
-	Params.MaxGen = 30;
-	Params.MutationProb = 0.2f;
+	Params.MutationCoeff = 0.1f;
+	Params.MutationProb = 0.1f;
 	Params.ParentCount = 2;
 
-	GAGNN Test(GNeuralNetwork(topology), 10, 4, 1.0f, 100, 1);
+	//Creaete a population of 50 networks of which 10 are the elites.
+	//Create 1000 random floats ~N(0, 1.0)
+	//Run in one worker thread
+	GAGNN Test(GNeuralNetwork(topology), 50, 10, 1000, 1);
+
+	//A variable to store the best network
 	NetworkWithInfo res;
 
 	cout.precision(3);
@@ -26,13 +35,17 @@ int main()
 	{
 		Test.CalcNextGeneration(Params, input, output);
 		res = Test.GetBest();
-		cout << res.Network.Info.Error << "\t|->\t";
-		cout << res.Network.Info.Accuracy << "\t" << res.ExInfo.Generation << "\t" << res.Network.Info.NumberOfWeights << "\t";
-		cout << Test.KillEliteAtRandom(0.01f);
-		cout << endl;
+		cout << res.Network.Info.Error << "\t|->\t"\
+			<< res.Network.Info.Accuracy << "\t" << res.ExInfo.Generation << "\t" << res.Network.Info.NumberOfWeights << "\t"\
+			<< endl;
 
 	}
-	cout << endl;
+
+	cout << "Max weight:" << res.Network.MaxWeightValue() << "\n"\
+	<< "Min weight:" << res.Network.MinWeightValue() << "\n"\
+	<< endl;
+
+	//For each input print the calculated output
 	for (int i = 0; i < input.size(); i++)
 	{
 		res.Network.Feed(input[i]);
