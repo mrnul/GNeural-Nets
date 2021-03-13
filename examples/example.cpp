07c1f09fa7a33daf3db6159fb9c80d8869235dfd
mrnul <16276083+mrnul@users.noreply.github.com>
2021-03-13 14:36:14 +0200

    Add files via upload

--- examples/example.cpp
+++ examples/example.cpp
@@ -4,13 +4,14 @@
 
 using std::cout;
 using std::endl;
+using std::cin;
 
 int main()
 {
 	//XOR inputs
 	const vector<vector<float>> input = { {1,0}, {1,1}, {0,1}, {0,0} };
 
-	//{1,0| = True
+	//{1,0} = True
 	//{0,1} = False
 	const vector<vector<float>> output = { {1,0}, {0,1}, {1,0}, {0,1} };
 
@@ -18,11 +19,11 @@ int main()
 	const vector<int> topology = { 2 , 2 , 2 };
 
 	GAGNNParams Params;
-	Params.MutationCoeff = 0.1f;
-	Params.MutationProb = 0.1f;
+	Params.MutationCoeff = 0.5f;
+	Params.MutationProb = 0.2f;
 	Params.ParentCount = 2;
 
-	//Creaete a population of 50 networks of which 10 are the elites.
+	//Create a population of 50 networks of which 10 are the elites.
 	//Create 1000 random floats ~N(0, 1.0)
 	//Run in one worker thread
 	GAGNN Test(GNeuralNetwork(topology), 50, 10, 1000, 1);
@@ -39,11 +40,13 @@ int main()
 			<< res.Network.Info.Accuracy << "\t" << res.ExInfo.Generation << "\t" << res.Network.Info.NumberOfWeights << "\t"\
 			<< endl;
 
+		// kill some in order to introduce some variance
+		Test.KillEliteAtRandom(0.1f);
 	}
 
 	cout << "Max weight:" << res.Network.MaxWeightValue() << "\n"\
-	<< "Min weight:" << res.Network.MinWeightValue() << "\n"\
-	<< endl;
+		<< "Min weight:" << res.Network.MinWeightValue() << "\n"\
+		<< endl;
 
 	//For each input print the calculated output
 	for (int i = 0; i < input.size(); i++)
@@ -51,4 +54,7 @@ int main()
 		res.Network.Feed(input[i]);
 		cout << GetOutputString(input[i]) << " --> " << res.Network.GetOutputString() << endl;
 	}
+
+	cout << "Done" << endl;
+	cin.get();
 }
